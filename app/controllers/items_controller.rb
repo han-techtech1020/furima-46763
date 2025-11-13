187492143654026782de806c9139cd0100281e4f
han-techtech1020 <kishohatano@gmail.com>
2025-11-13 16:10:29 +0900

    ストロングパラメーター

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,12 @@ class ItemsController < ApplicationController
   def new
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, 
+                                :shipping_charge_id, :prefecture_id, :shipping_day_id, 
+                                :price, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,14 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
+  has_one_attached :image
+
   with_options presence: true do
     validates :name, length: { maximum: 40 }
     validates :description, length: { maximum: 1000 }
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "must be between ¥300 and ¥9,999,999" }
     validates :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id
+    validates :image
   end
 
   validates :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id,
