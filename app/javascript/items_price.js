1af30bcc0fe0f167805b68d13b8b3dd1dd815abd
han-techtech1020 <kishohatano@gmail.com>
2025-11-13 17:25:12 +0900

    javascript

---
+++ app/javascript/items_price.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', function() {
+  const priceInput = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  if (priceInput) {
+    priceInput.addEventListener("input", () => {
+      let value = parseInt(priceInput.value);
+      if (!isNaN(value)) {
+        const tax = Math.floor(value * 0.1);
+        const profitValue = value - tax;
+        addTaxPrice.innerHTML = tax;
+        profit.innerHTML = profitValue;
+      } else {
+        addTaxPrice.innerHTML = 0;
+        profit.innerHTML = 0;
+      }
+    });
+  }
+});
