8647e31aab6f006f31707eacb327509d5ebd8f44
han-techtech1020 <kishohatano@gmail.com>
2025-11-15 12:09:39 +0900

    erb編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-    resources :purchase, only: [:new, :create]
+    resources :purchases, only: [:new, :create]
   end
 end
