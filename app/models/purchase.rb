e12a031b4852af4faa9cd7d9cac31d0b0f6b8fdb
han-techtech1020 <kishohatano@gmail.com>
2025-11-16 10:47:21 +0900

    エラー文

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,7 +4,9 @@ class Purchase
   attr_accessor :postal_code, :prefecture_id, :city, :street, :building, :phone_number, :user_id, :item_id, :token
 
   # バリデーション
-  validates :postal_code, :prefecture_id, :city, :street, :phone_number, :token, presence: true
+  validates :postal_code, :city, :street, :phone_number, :token, presence: true
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
   validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
   validates :phone_number, format: { with: /\A\d{10,11}\z/ }
 
